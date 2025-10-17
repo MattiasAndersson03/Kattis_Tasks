bde6aaebe82b16698051cf6de03f8b65c23f65c2
Mattias Andersson <mattias0306@outlook.com>
2025-10-17 23:44:13 +0200

    Add comment to clarify shortcut calculation in shortcuttowhat.py

--- Python/Easy/shortcuttowhat.py
+++ Python/Easy/shortcuttowhat.py
@@ -1,3 +1,3 @@
 a = int(input())
-total= ((a + 5) * 3) - 10
+total= ((a + 5) * 3) - 10 # Calculates the shortcut to the desired number
 print(total) # Calculates the shortcut to the desired number and prints it
