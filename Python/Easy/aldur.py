6b0a667defe50ef15a1f93f9290ce132fdd99519
Mattias Andersson <mattias0306@outlook.com>
2025-10-20 22:43:23 +0200

    Update comment to clarify input reading in aldur.py

--- Python/Easy/aldur.py
+++ Python/Easy/aldur.py
@@ -2,6 +2,6 @@ a = int(input())  # Reads an integer input representing the count of numbers
 numbers = []
 
 for _ in range(a): 
-    num = int(input()) # Reads each integer input 
+    num = int(input()) # Reads each number input 
     numbers.append(num) # Appends the number to the list
 print(min(numbers)) # Prints the smallest number from the list
