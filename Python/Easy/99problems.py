c6f9420b75e66751b960b1df2aa9d942562bb494
Mattias Andersson <mattias0306@outlook.com>
2025-11-16 22:45:42 +0100

    Fix typo in comment and add newline at the end of 99problems.py

--- Python/Easy/99problems.py
+++ Python/Easy/99problems.py
@@ -3,4 +3,4 @@ total = format.round(a)
 print(total - 1) # Rounds the input number and prints it
 # The code reads an integer input, rounds it to the nearest whole number, subtracts one, and prints the result.
 
-# Importatnt do not work
+# Importatnt do not work!
